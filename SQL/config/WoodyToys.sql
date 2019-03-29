031e91b206e4f9a9fa47f8c4d368a0b5d0e6fbe0
domad007 <d.fiedorczuk@students.ephec.be>
2019-03-29 16:11:17 +0100

    Commentaires dans SQL

--- SQL/config/WoodyToys.sql
+++ SQL/config/WoodyToys.sql
@@ -1,6 +1,8 @@
+--creation de la BDD
 create database WoodyToys;
 use WoodyToys;
 
+-- creation de la table Client
 CREATE TABLE Client
 (
 ID_Client int not null auto_increment,
@@ -9,6 +11,7 @@ ADRESSE text not null,
 PRIMARY KEY (ID_Client)
 );
 
+--creation de la table Produit
 CREATE TABLE Produit
 (
 ID_PRODUIT int not null auto_increment,
@@ -17,7 +20,7 @@ PRIX int not null,
 PRIMARY KEY (ID_PRODUIT)
 );
 
-
+--creation de la table Commande
 CREATE TABLE Commande
 (
 ID_COMMANDE int not null auto_increment,
@@ -27,14 +30,17 @@ PRIMARY KEY (ID_COMMANDE, ID_Client),
 FOREIGN KEY (ID_Client) REFERENCES Client(ID_Client)
 );
 
+-- creation des utilisateurs
 CREATE USER 'servWeb'@'%' IDENTIFIED BY '';
 CREATE USER 'employe'@'localhost' IDENTIFIED BY 'emp1';
 CREATE USER 'maintenance'@'localhost' IDENTIFIED BY 'admin';
 CREATE USER 'root'@'%' IDENTIFIED BY '';
 
+-- on donne les privileges aux utilisateurs
 GRANT ALL PRIVILEGES ON * . * TO 'servWeb'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'maintenance'@'localhost';
 GRANT ALL PRIVILEGES ON * . * TO 'root'@'%';
 GRANT SELECT ON *.* TO 'employe'@'localhost';
 
+--on applique les modifications apportes aux utilisateurs
 FLUSH PRIVILEGES;
